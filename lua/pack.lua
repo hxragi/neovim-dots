f346c6ad8937816056534e4ffe54ed4cf1616fc4
hxragi <mixintrace@gmail.com>
2026-06-15 03:01:40 +0300

    add many plugins and etc.

--- lua/pack.lua
+++ lua/pack.lua
@@ -6,10 +6,15 @@ vim.pack.add({
   'https://github.com/nvim-mini/mini.statusline',
   'https://github.com/nvim-treesitter/nvim-treesitter',
   'https://github.com/neovim/nvim-lspconfig',
-  { src = 'https://github.com/Saghen/blink.cmp', branch = "v1" },
+  { src = 'https://github.com/Saghen/blink.cmp', branch = "v2" },
+  'https://github.com/saghen/blink.lib',
   'https://github.com/nvim-telescope/telescope.nvim',
   'https://github.com/nvim-lua/plenary.nvim',
-  'https://github.com/nvim-telescope/telescope-fzf-native.nvim'
+  'https://github.com/nvim-telescope/telescope-fzf-native.nvim',
+  'https://github.com/windwp/nvim-autopairs',
+  'https://github.com/folke/noice.nvim',
+  'https://github.com/MunifTanjim/nui.nvim',
+  'https://github.com/vyfor/cord.nvim',
 })
 
 --- catppuccin theme ---
@@ -35,13 +40,13 @@ require('mini.statusline').setup({
     active = function()
       local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
       local git           = MiniStatusline.section_git({ trunc_width = 75 })
-      local diagnostics    = MiniStatusline.section_diagnostics({ trunc_width = 75 })
-      local filename       = MiniStatusline.section_filename({ trunc_width = 140 })
-      local location       = MiniStatusline.section_location({ trunc_width = 75 })
+      local diagnostics   = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+      local filename      = MiniStatusline.section_filename({ trunc_width = 140 })
+      local location      = MiniStatusline.section_location({ trunc_width = 75 })
 
       return MiniStatusline.combine_groups({
-        { hl = mode_hl,                  strings = { mode } },
-        { hl = "MiniStatuslineDevinfo",  strings = { git, diagnostics } },
+        { hl = mode_hl,                 strings = { mode } },
+        { hl = "MiniStatuslineDevinfo", strings = { git, diagnostics } },
         "%<",
         { hl = "MiniStatuslineFilename", strings = { filename } },
         "%=",
@@ -70,8 +75,61 @@ vim.api.nvim_create_autocmd("FileType", {
 
 --- blink cmp ---
 require('blink-cmp').setup({
-  keymap = {
-    preset = "default",
-    ["<CR>"] = { "accept", "fallback" },
+  keymap = { preset = 'enter' },
+  appearance = { nerd_font_variant = 'mono' },
+  completion = {
+    documentation = { auto_show = false, auto_show_delay_ms = 5000 },
+    menu = {
+      border = "rounded",
+      winhighlight =
+        "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
+    },
+    trigger = { prefetch_on_insert = false },
+  },
+  signature = {
+    enabled = false,
+    trigger = {
+      enabled = false,
+      show_on_trigger_character = false,
+      show_on_insert_on_trigger_character = false,
+    },
+  },
+  sources = {
+    default = { 'lsp', 'path', 'snippets', 'buffer' },
   },
 })
+
+--- autopairs ---
+require("nvim-autopairs").setup()
+
+--- noice ---
+require("noice").setup({
+  lsp = {
+    override = {
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      ["vim.lsp.util.stylize_markdown"] = true,
+      ["cmp.entry.get_documentation"] = true,
+    },
+    hover = {
+      enabled = true,
+      view = "hover",
+    },
+    signature = {
+      enabled = true,
+    },
+  },
+  presets = {
+    bottom_search = true,
+    command_palette = true,
+    long_message_to_split = true,
+    lsp_doc_border = true,
+  },
+})
+
+--- cord ---
+require('cord').setup {
+  editor = { tooltip = "💻 Neovim <3" },
+  idle = { enabled = false },
+  display = { theme = "catppuccin" },
+}
+
